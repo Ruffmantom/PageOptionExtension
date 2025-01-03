a5b09597c6cd16e8a9fea13f53dfa9b395b734cb
Tom Ruff <ruffmantom@gmail.com>
2025-01-03 16:04:31 -0700

    added some styles

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,6 +1,40 @@
 let globalStore = {
     productTypes: [],
-    options: [],
+    settings: {
+        useIndexInName: false,
+    }
+}
+
+const exampleData = {
+    productTypes: [
+        {
+            id:1,
+            name: 'Vinyl Banner Type',
+            options:[
+                {
+                    id:1,
+                    name: 'Roll Materials',
+                    index:1,
+                    rename:"",
+                    sortNumber:0,
+                },
+                {
+                    id:2,
+                    name: 'Ink',
+                    index:2,
+                    rename:"",
+                    sortNumber:0,
+                },
+                {
+                    id:3,
+                    name: 'Grommets',
+                    index:3,
+                    rename:"",
+                    sortNumber:0,
+                },
+            ]
+        }
+    ],
     settings: {
         useIndexInName: false,
     }
